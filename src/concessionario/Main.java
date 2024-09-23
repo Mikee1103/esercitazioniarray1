aadef7559c4fe70969af9e9037fd281a030a98c5
seghezzi.21139 <seghezzi.michele.studente@itispaleocapa.it>
2024-09-23 13:39:32 +0200

    commit 3

--- src/concessionario/Main.java
+++ src/concessionario/Main.java
@@ -3,7 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in= new Scanner(System.in);
+        int indice;
         int scelta;
+        String AggiungiMarca, AggiungiModello;
+        double AggiungiPrezzo;
+        String[] modello= new String[100];
+        String[] marca= new String[100];
+        double[] prezzo= new double[100];
+        int conta =0;
         do {
             System.out.println("1-aggiungi auto");
             System.out.println("2-visualizza le auto");
@@ -13,13 +20,34 @@ public class Main {
             System.out.println("6-visualizza le auto in ordine crescente");
             System.out.println("7-visualizza tutti i modelli diesel");
             System.out.println("8-esci");
-            scelta=in.nextInt();
+            scelta=Integer.parseInt(in.next());
             switch (scelta) {
                 case 1:
-
+                    System.out.println("dammi una marca");
+                    AggiungiMarca = in.next();
+                    System.out.println("dammi un modello");
+                    AggiungiModello = in.next();
+                    System.out.println("dammi un prezzo");
+                    AggiungiPrezzo = Double.parseDouble(in.next());
+                    funzioni.aggiunta(AggiungiMarca, AggiungiModello, AggiungiPrezzo, modello, marca, prezzo, conta);
+                    conta++;
+                    break;
                 case 2:
-
+                     System.out.println(funzioni.visualizzaTutto(marca, modello, prezzo, conta));
+                     break;
                 case 3:
+                    System.out.println("dammi una marca");
+                    AggiungiMarca = in.next();
+                    System.out.println("dammi un modello");
+                    AggiungiModello = in.next();
+                    indice=funzioni.ricerca(AggiungiMarca, AggiungiModello, modello, marca, conta);
+                    if(indice==-1)
+                        System.out.println("non esistono marca e modello che coincidono");
+                    else
+                        System.out.println("L'auto esiste");
+                        AggiungiPrezzo= prezzo[indice];
+                        System.out.println(AggiungiMarca+" "+AggiungiModello+" "+AggiungiPrezzo);
+                        break;
 
                 case 4:
 
--- src/concessionario/funzioni.java
+++ src/concessionario/funzioni.java
@@ -1,3 +1,23 @@
 package concessionario;
 public class funzioni {
+    public static void aggiunta (String AggiungiMarca, String AggiungiModello, double AggiungiPrezzo, String[] modello, String[] marca, double[] prezzo, int conta){
+        modello[conta]= AggiungiModello;
+        marca[conta]=AggiungiMarca;
+        prezzo[conta]=AggiungiPrezzo;
+    }
+    public static int ricerca (String AggiungiMarca, String AggiungiModello, String[] modello, String[] marca, int conta) {
+        for (int i = 0; i < conta; i++) {
+            if (AggiungiMarca.equals(marca[i]) && (AggiungiModello.equals(modello[i]))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static String visualizzaTutto(String[] marca, String[] modello, double[] prezzo, int conta){
+        String tutto= "";
+        for(int i=0; i<conta; i++){
+            tutto+= marca[i]+" "+modello[i]+" "+prezzo[i]+"\n";
+        }
+        return tutto;
+    }
 }
